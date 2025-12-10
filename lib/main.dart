0f65864d43a93715e9202b24302b9355f61278fd
Hina765 <hinav325@gmail.com>
2025-12-10 11:17:23 +0530

    budget tracker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:expense_tracker/screens/splash_screen.dart';
+import 'package:expense_tracker/screens/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
